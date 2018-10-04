ddf8851d7fd28a939dae9a474d5c61cd9737bee8
Scott Bowler <scott.bowler@learningfuze.com>
2018-10-04 11:27:35 -0700

    Added home page

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import '../assets/css/app.css';
 import React from 'react';
 import { NavLink, Route } from 'react-router-dom';
+import Home from './home';
 import NormalExample from './normal_example';
 import EasyExample from './easy_example';
 
@@ -9,9 +10,11 @@ const App = () => (
         <h1 className="center">Accordion Example</h1>
         <ul className="nav center">
             <li><NavLink to="/true-accordion">View True Accordion</NavLink><div/></li>
+            <li><NavLink to="/" exact>Home</NavLink><div /></li>
             <li><NavLink to="/easy-accordion">View Easier Accordion</NavLink><div/></li>
         </ul>
         
+        <Route path="/" exact component={Home}/>
         <Route path="/true-accordion" component={NormalExample}/>
         <Route path="/easy-accordion" component={EasyExample}/>
     </div>
---
+++ src/components/home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => (
+    <p className="description center">Choose an example to view above. Each example contains a brief description.</p>
+)
